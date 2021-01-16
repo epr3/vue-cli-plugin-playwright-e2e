a03bd79be657baf0230d8268482f7c165eee3a2e
Eduard Predescu <eduard.florin.predescu@gmail.com>
2021-01-16 19:51:54 +0200

    initial scaffolding

---
+++ presets/typescript/jest-preset.js
@@ -0,0 +1,9 @@
+const deepmerge = require("deepmerge");
+const defaultPreset = require("../default/jest-preset");
+
+module.exports = deepmerge(defaultPreset, {
+  moduleFileExtensions: ["ts", "tsx"],
+  transform: {
+    "^.+\\.tsx?$": require.resolve("ts-jest"),
+  },
+});
